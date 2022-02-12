ee264286bdbe325dd80aae37482c137c586dbba0
msimbocoders <coders@msimboit.net>
2022-02-13 01:07:19 +0300

    updated the db connection

--- db.js
+++ db.js
@@ -1,10 +1,10 @@
 const Pool = require("pg").Pool;
 const pool = new Pool({
   user: "postgres",
-  password: "root",
+  password: "",
   host: "localhost",
   port: 5432,
-  database: "todo",
+  database: "menulist",
 });
 
 module.exports = pool;
